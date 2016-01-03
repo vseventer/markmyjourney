f1e9a73740e7a9e469fdbe9e4d9f32f898c4cf19
vseventer <mark@vseventer.com>
2016-01-03 21:45:08 +1100

    Update templates, dependencies.

--- bin/exif.js
+++ bin/exif.js
@@ -25,7 +25,7 @@
 // Strict mode.
 'use strict';
 
-// Local modules.
+// Standard lib.
 var fs   = require('fs'),
     path = require('path');
 
@@ -33,6 +33,9 @@ var fs   = require('fs'),
 var exifReader = require('exif-reader'),
     moment     = require('moment');
 moment.defaultFormat = 'YYYY-MM-DD[T]HH:mm:ssZ'; // Retain timezone.
+
+// Local modules.
+var metadata = require('../uploads/metadata.json');
 
 // Configure.
 var images = process.argv.slice(2);
@@ -63,20 +66,30 @@ var res = images.map(function(image) {
 
 // Extract fields from EXIF.
 res = res.map(function(data, index) {
-  var d = moment.parseZone(data.exif.DateTimeOriginal); // Retain timezone.
-  return {
-    type: 'Feature',
-    properties: {
-      src  : path.basename(images[index]),
-      date : d.format()
-    },
-    geometry: {
+  var d    = moment.parseZone(data.exif.DateTimeOriginal), // Retain timezone.
+      name = path.basename(images[index]);
+
+  // Format geometry.
+  var geometry = null;
+  if(null != data.gps && null != data.gps.GPSLongitude && !isNaN(data.gps.GPSLongitude[0])) {
+    geometry = {
       type: 'Point',
       coordinates: [
         dmsToDd(data.gps.GPSLongitude, data.gps.GPSLongitudeRef),
         dmsToDd(data.gps.GPSLatitude,  data.gps.GPSLatitudeRef)
       ]
     }
+  }
+
+  // Return the feature.
+  return {
+    type: 'Feature',
+    properties: {
+      src  : name,
+      date : d.format(),
+      description: metadata[name] || null
+    },
+    geometry: geometry
   };
 });
 
